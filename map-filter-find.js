b4bc183363c3e5474f494d2a8301084bcb4a95cb
sabbir-320 <sabbir195661@gmail.com>
2020-08-15 11:11:43 +0600

    map

--- map-filter-find.js
+++ map-filter-find.js
@@ -12,19 +12,26 @@ for (i = 0; i < getSquareNum.length; i++) {
 }
 
 //by map()
-const result = getSquareNum.map(x => x * x)
-console.log();
+const result1 = getSquareNum.map(x => x * x)
 
-// filter method
-const result = getSquareNum.filter(x => x > 5)
-console.log();
+
+// filter method = for some select
+const result2 = getSquareNum.filter(x => x > 5)
+
 
 // How to get specific common property from an array that has many object
 const students = [
     { id: "1", name: "Omor"},
     { id: "2", name: "Ali"}
-    
 ]
+
+const result = [];
+for(let i = 0; i < students.length; i++){
+    const element = students[i]
+    result.push(element.name);
+    
+}
+// console.log(result);
+
 const name = students.map( x => x.name)
 const ids = students.filter( x => x.id < 2)
-console.log();
---
+++ scope.js
@@ -0,0 +1,9 @@
+// scope
+
+// local scope 
+function muFun(){
+    const num = 2+2;
+    console.log(num);
+}
+
+// global scope
